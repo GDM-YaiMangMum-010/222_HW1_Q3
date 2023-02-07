9f787a6b3c1de12d6c0b8887e097e44f1151f292
Jirayu_SBTR <jirayu.majesty@mail.kmutt.ac.th>
2023-02-07 21:09:29 +0700

    F(x)ติดSqrt

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+class Program
+{
+    static void Main (string[] args)
+    {
+        double x = 0;
+        x = double.Parse(Console.ReadLine());
+        Console.WriteLine(F(x));
+    }
+    static double F (double x)
+    {
+        if ( x > 1 )
+        {
+            return F( x - 2 );
+        }
+        else if ( 0 < x && x <= 1 )
+        {
+            return -(Math.Sqrt(1-Math.Pow(x,2)));
+        }
+        else if ( -1 < x && x <= 0 )
+        {
+            return Math.Sqrt(1-Math.Pow(x,2));
+        }
+        else
+        {
+            return F( x + 2 );
+        }
+    }
+}
